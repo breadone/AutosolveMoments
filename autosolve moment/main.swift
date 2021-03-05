2b4d8e2d3c571a77624e7204c45743bf1a2f2c1c
pradyun <pradyun.setti@gmail.com>
2021-03-05 20:44:13 +1300

    added most of force class, begun adding y and x component calculatoin

--- autosolve moment/main.swift
+++ autosolve moment/main.swift
@@ -7,5 +7,32 @@
 
 import Foundation
 
-print("Hello, World!")
+class Force {
+    var mag, xComponent, yComponent, angle, pDist: Double
+    
+    init(mag: Double, angle: Double, pDist: Double) {
+        self.mag = mag //magnitude of vector
+        self.angle = angle //angle of vector !!FROM POS X AXIS!!
+        self.pDist = pDist
+        self.xComponent = 0
+        self.yComponent = 0
+        CalcDist()
+    }
+    
+    func CalcDist() {
+        switch self.angle {
+        case ...90:
+            self.xComponent = self.mag * cos(self.angle)
+            self.yComponent = self.mag * cos(self.angle)
+        case 90...180:
+            let rAngle = 180 - self.angle
+            self.xComponent = -(self.mag * cos(rAngle))
+            self.yComponent = self.mag * cos(rAngle)
+        default:
+            print("invalid angle")
+        }
+        
+    }
+    
+}
 
