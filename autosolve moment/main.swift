8983dcb4bf6723937c7d66d844be22bb5d807bf8
pradyun <pradyun.setti@gmail.com>
2021-03-06 19:30:28 +1300

    added x and y distance components changed pDist to dDIst for direct distance
    still not working, im using the wrong angles on lines 47 and 48

--- autosolve moment/main.swift
+++ autosolve moment/main.swift
@@ -8,19 +8,21 @@
 import Foundation
 
 class Force {
-    var mag, xComponent, yComponent, angle, pDist: Double
+    var mag, xComponent, yComponent, angle, dDist, xDist, yDist: Double
     
-    init(mag: Double, angle: Double, pDist: Double) {
+    init(mag: Double, angle: Double, dDist: Double) {
         self.mag = mag //magnitude of vector
         self.angle = angle //angle of vector in degrees !!FROM POS X AXIS!!
-        self.pDist = pDist //perpendicular distance to pivot
+        self.dDist = dDist //direct distance to pivot
         self.xComponent = 0
         self.yComponent = 0
+        self.xDist = 0
+        self.yDist = 0
         CalcComponents()
     }
     
     func CalcComponents() {
-        self.angle = (self.angle * Double.pi / 180) //convert into radians for swift
+        self.angle = (self.angle * π / 180) //convert into radians for swift
         
         switch self.angle {
         case 0...π/2: //A quadrant
@@ -41,24 +43,27 @@ class Force {
         default:
             print("invalid angle")
         }
+        
+        self.xDist = self.dDist * cos(self.angle)
+        self.yDist = self.dDist * sin(self.angle)
     }
 }
 
 var Forces = [Force]() //creates empty array of forces
 
-while true {
-    print("Enter the new Force's magnitude: ")
+while true { //inputs force(s)
+    print("Enter the new Force's magnitude: ", terminator: "")
     let newMag = Double(readLine() ?? "0")!
     
-    print("Enter the new Force's angle, in degrees, from posX axis: ")
+    print("Enter the new Force's angle, in degrees, from posX axis: ", terminator: "")
     let newAngle = Double(readLine() ?? "0")!
     
-    print("Enter the new Force's perpendicular distance from the pivot: ")
-    let newpDist = Double(readLine() ?? "0")!
+    print("Enter the new Force's direct distance from the pivot: ", terminator: "")
+    let newdDist = Double(readLine() ?? "0")!
     
-    Forces.append(Force(mag: newMag, angle: newAngle, pDist: newpDist))
+    Forces.append(Force(mag: newMag, angle: newAngle, dDist: newdDist))
         
-    print("Add another Force? (y/n): ")
+    print("Add another Force? (y/n): ", terminator: "")
     if readLine() == "n" {
         break
     }
@@ -67,8 +72,8 @@ while true {
 var sumX: Double = 0
 var sumY: Double = 0
 for i in 0...Forces.count - 1 {
-    sumX += Forces[i].xComponent.roundToPlaces(toPlaces: 3)
-    sumY += Forces[i].yComponent.roundToPlaces(toPlaces: 3)
+    sumX += Forces[i].xComponent * Forces[i].yDist
+    sumY += Forces[i].yComponent * Forces[i].xDist
 }
-
-print("X: \(sumX)\nY: \(sumY)")
+print("-------")
+print("∑X: \(sumX.roundToPlaces(toPlaces: 2))\n∑Y: \(sumY.roundToPlaces(toPlaces: 2))\nTotal: \((sumX+sumY).roundToPlaces(toPlaces: 3))")
